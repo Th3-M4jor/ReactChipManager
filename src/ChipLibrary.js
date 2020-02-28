d81359913292b7f9e6ba95c2f5ccd1f797951fcd
The Major <spartan364@gmail.com>
2020-02-28 14:40:59 -0800

    replaced var with let

--- src/ChipLibrary.js
+++ src/ChipLibrary.js
@@ -35,7 +35,7 @@ function storageAvailable(type) {
     var storage;
     try {
         storage = window[type];
-        var x = '__storage_test__';
+        let x = '__storage_test__';
         storage.setItem(x, x);
         storage.removeItem(x);
         return true;
